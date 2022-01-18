fd55d3aa1e487a6fd96f166351c9ab714784d9c0
sakai-code <76587090+sakai-code@users.noreply.github.com>
2022-01-18 19:45:57 +0900

    test upload

--- custom.ts
+++ custom.ts
@@ -1,34 +1,31 @@
+enum TempConvertType {
+    //% block="°C → °F"
+    CtoF,
+    //% block="°F → °C"
+    FtoC,
+}
 
-
-
-
-
-
-
-
-
-
-//% color="#4C97FF"
-namespace blocks {
-
-    let test: () => void;
-    //%block
-    export function move(): void {
-
-        test();
-
+//%color=#2bd92e weight=95 icon="\uf0ad"
+namespace myBlocks {
+    //% blockId=myBlocks_convertTemperature
+    //% block="convert|%value|%convertType"
+    export function convertTemperature(value: number, convertType: TempConvertType): number {
+        if (convertType === TempConvertType.CtoF) {
+            return cToF(value);
+        }
+        if (convertType === TempConvertType.FtoC) {
+            return fToC(value);
+        }
+        return value;
     }
-    /**
-     * This is an event handler block
-     */
-    //% block="on event"
-    //% draggableParameters="reporter"
-    export function onEvent(handler: () => void) {
-        handler = test;
 
-        let a = 0;
+    //% shim=myBlocks::cToF
+    export function cToF(value: number) {
+        return 32.0; // dummy for sim
+    }
 
-
-
+    //% shim=myBlocks::fToC
+    export function fToC(value: number) {
+        return -17.78; // dummy for sim
     }
 }
---
+++ myBlocks.cpp
@@ -0,0 +1,26 @@
+#include "pxt.h"
+
+
+
+
+
+namespace myBlocks
+{
+    //%
+    float cToF(float value)   
+    {  
+
+    uBit.display.scroll("HELLO WORLD!",50);
+
+    release_fiber();
+    }
+
+    //%
+    float fToC(float value)
+    {   
+        return (value - 32.0) / 1.8;
+    }
+} // namespace myBlocks
+// Documentation on defining PXT blocks:
+// https://www.pxt.io/defining-blocks
+
