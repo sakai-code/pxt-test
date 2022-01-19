f7a36ebb2d37a5babc6f4655535141adc1400095
sakai-code <76587090+sakai-code@users.noreply.github.com>
2022-01-19 09:13:15 +0900

    更新 pxt.json, main.ts, myBlocks.cpp, enums.d.ts, shims.d.ts

--- myBlocks.cpp
+++ myBlocks.cpp
@@ -3,19 +3,32 @@
 
 
 
-
+//% weight=100 color=#0fbc11 
+//% icon="\uf110"
 namespace myBlocks
 {
-    //%block
-    void cToF(float value)   
+    
+ 
+  
+      /**
+     * Draws an image on the LED screen.
+     * @param leds the pattern of LED to turn on/off
+     * @param interval time in milliseconds to pause after drawing
+     */
+    //% help= myBlocks/cToF
+    //% weight=95 blockGap=8
+    //% blockId=CTOF
+    //% block="CTOF" icon="\uf00a"
+    int  cToF()   
     {  
 
     uBit.display.scroll("HELLO WORLD!",50);
 
     release_fiber();
+    return 0;
     }
 
-    //%
+  
     float fToC(float value)
     {   
         return (value - 32.0) / 1.8;
---
+++ shims.d.ts
@@ -0,0 +1,21 @@
+// Auto-generated. Do not edit.
+
+
+
+    //% weight=100 color=#0fbc11 
+    //% icon="\uf110"
+declare namespace myBlocks {
+
+    /**
+     * Draws an image on the LED screen.
+     * @param leds the pattern of LED to turn on/off
+     * @param interval time in milliseconds to pause after drawing
+     */
+    //% help= myBlocks/cToF
+    //% weight=95 blockGap=8
+    //% blockId=CTOF
+    //% block="CTOF" icon="\uf00a" shim=myBlocks::cToF
+    function cToF(): int32;
+}
+
+// Auto-generated. Do not edit. Really.
