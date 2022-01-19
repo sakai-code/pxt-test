6577a25a2389cb7169c1e708c825d20c05f5715d
sakai-code <76587090+sakai-code@users.noreply.github.com>
2022-01-19 10:39:30 +0900

    更新 myBlocks.cpp

--- myBlocks.cpp
+++ myBlocks.cpp
@@ -5,27 +5,21 @@
 
 
 namespace myBlocks
-{ 
+{
+  
    
-    void ctof( )   
+    void cToF()   
     {  
 
     uBit.display.scroll("HELLO WORLD!",50);
 
     release_fiber();
-
     }
-   
 
-    
-    
-  
-    
     float fToC(float value)
     {   
         return (value - 32.0) / 1.8;
     }
-} // namespace myBlocks
-// Documentation on defining PXT blocks:
-// https://www.pxt.io/defining-blocks
+} 
+
 
